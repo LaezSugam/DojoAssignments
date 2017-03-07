70a69b3a5ece3da880cb6d562f1ccb9e182ac429
Bill Peterson <william.o.peterson@gmail.com>
2017-03-06 16:33:29 -0800

    started python

--- WebFundamentals/Javascript/thrones/thrones.js
+++ WebFundamentals/Javascript/thrones/thrones.js
@@ -3,9 +3,8 @@ $(document).ready(function() {
     $('form').submit(function() {
         var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + $("input[name=city]").val() + "&appid=ceb7be7ea564d604e66ee258802b85c7";
         $.get(apiURL, function(data) {
-        	console.log(data);
              var tempF = Math.trunc((data.main.temp - 273.15)*1.8 + 32);
-             var newHTML = "<h1>" + data.name + ", " + data.sys.country + "</h1><p>Temperature: " + tempF + "°</p>";
+             var newHTML = "<h1>" + data.name + "</h1><p>Temperature: " + tempF + "°</p>";
              $("#weatherBox").html(newHTML);
         }, 'json');
         // don't forget to return false so the page doesn't refresh
